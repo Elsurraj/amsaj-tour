3b5931640f3f4d51ef52b072d210dc725c756095
Surraj AbubaKhar <95746643+Elsurraj@users.noreply.github.com>
2022-07-01 12:52:54 -0700

    Add files via upload
    initial add amsaj-tour file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//js for handbugger Menu
+var menuItems=document.getElementById("menuItems");
+ menuItems.style.maxHeight='0px';
+
+function menutoggle(){
+    if(menuItems.style.maxHeight=='0px'){
+     menuItems.style.maxHeight='200px';
+    }
+    else{
+        menuItems.style.maxHeight='0px';
+    }
+}
